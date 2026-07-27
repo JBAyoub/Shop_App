1d96345d73f8eeb9f965901038ff937c4d9e6dc3
Ayb <aymojobe@gmail.com>
2026-07-27 16:08:24 +0100

    chips navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,13 @@ class ShopApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: "Lato",
         useMaterial3: true,
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(fontWeight: .bold, fontSize: 16),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 0.6),
+        ),
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromRGBO(256, 204, 1, 1),
+          seedColor: const Color.fromRGBO(254, 204, 1, 1),
+          primary: const Color.fromRGBO(254, 204, 1, 1),
         ),
       ),
     );
--- lib/shop_screen.dart
+++ lib/shop_screen.dart
@@ -8,8 +8,95 @@ class ShopScreen extends StatefulWidget {
 }
 
 class _ShopScreenState extends State<ShopScreen> {
+  final List<String> _filters = const ["All", "Nike", "Adidas", "Bata"];
+  late String _selectedFilter;
+  @override
+  void initState() {
+    super.initState();
+    _selectedFilter = _filters[0];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    const outlineInputBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
+      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1), width: 2),
+    );
+    const errorBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
+      borderSide: BorderSide(color: Color.fromRGBO(245, 116, 116, 1), width: 2),
+    );
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    'Shoes\nCollection',
+                    style: TextStyle(fontWeight: .bold, fontSize: 35),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    autocorrect: true,
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      prefixIcon: Icon(Icons.search_sharp),
+                      border: outlineInputBorder,
+                      enabledBorder: outlineInputBorder,
+                      focusedBorder: outlineInputBorder,
+                      errorBorder: errorBorder,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ListView.builder(
+                  scrollDirection: .horizontal,
+                  itemCount: _filters.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedFilter = _filters[index];
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Chip(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            side: const BorderSide(
+                              width: 0.5,
+                              color: Color.fromRGBO(245, 247, 249, 1),
+                            ),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          backgroundColor: _selectedFilter == _filters[index]
+                              ? Theme.of(context).colorScheme.primary
+                              : const Color.fromARGB(255, 235, 235, 235),
+                          label: Text(_filters[index]),
+                          labelStyle: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
