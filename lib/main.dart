3299d52b6dc860107c3e46dbb82f020eb25f4ad4
Ayb <aymojobe@gmail.com>
2026-07-27 14:11:23 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/shop_screen.dart';
 
+void main() {
+  runApp(const ShopApp());
+}
+
+class ShopApp extends StatelessWidget {
+  const ShopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      routes: {
+        "shop_screen": (context) => ShopScreen(),
+        "cart": (context) => ShopScreen(),
+      },
+      initialRoute: "shop_screen",
+      home: ShopScreen(),
+      theme: ThemeData(
+        fontFamily: "Lato",
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(256, 204, 1, 1),
+        ),
+      ),
+    );
+  }
+}
